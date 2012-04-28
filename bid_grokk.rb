e4217f4717b815c3671a1c7a17277f093a61512e
Chris Shreve <c.k.shreve@gmail.com>
2012-04-28 10:34:12 -0700

    removed extra method

--- bid_grokk.rb
+++ bid_grokk.rb
@@ -1,27 +1,27 @@
 class BidGrokk
-  def main (servers, bids)
-    #puts "servers = #{servers}, bids = #{bids}"
+
+  def main (servers, bids
+    # use servers as max length and bids as decreasing y values
+    # finding target y and max area is then equivalent to finding target bid and maximum money
     target_bid, max_bling = find_max_area servers, bids
     puts "target bid = #{target_bid}, max bling = #{max_bling}"
   end
 
-  def find_best_bid (servers, bids)
-    find_max_area servers, bids
-  end
-
-  def find_max_area (length, y_values)
-    max_area, target_height = 0, 0
+  # given max length and decreasing y values, find rectangle with maximum area
+  def find_max_area (max_length, y_values)
+    max_area, target_y = 0, 0
 
     y_values.each_with_index { |height, i|
-      width = [i+1, length].min
+      width = [i+1, max_length].min
       current_area = height * width
 
       if current_area > max_area
         max_area = current_area
-        target_height = height
+        target_y = height
       end
     }
 
-    [target_height, max_area]
+    [target_y, max_area]
   end
+
 end
